d8a4c69968e39e42717bb52755337e8f60a3dee2
atlantisq <peterqin315@gmail.com>
2023-06-05 23:02:33 -0700

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sat May 13 00:22:21 2023
+
+@author: atlan
+"""
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Dec 14 21:58:48 2021
 
 @author: atlan
 """
 import random
 import math
+import copy
 
 monomer_list = []
 shuffled_monomer_list = []
@@ -38,34 +45,135 @@ current_parameters = {
     'TFK_number' : 0,
     'initiator_number' : 10,
 
-    'k_transfer_to_ttc' : 20,
-    'k_transfer_to_TFK' : 20,
+    'k_transfer_to_ttc' : 220,
+    'k_transfer_to_TFK' : 220,
     
     'r1' : 1,
     'r2' : 1
      }
 
 batch_exprmnts = [
-        
     {
-    'monomer_number' : 1500,
-    'crosslinker_number' : 375,
+    'monomer_number' : 15000,
+    'crosslinker_number' : 5000,
     'TTC_number' : 0,
-    'TFK_number' : 125,
+    'TFK_number' : 0,
+    'initiator_number' : 62,
+     },
+    
+    
+    {
+    'monomer_number' : 15000,
+    'crosslinker_number' : 4750,
+    'TTC_number' : 0,
+    'TFK_number' : 250,
+    'initiator_number' : 62,
+     },
+                  
+     {
+    'monomer_number' : 15000,
+    'crosslinker_number' : 4250,
+    'TTC_number' : 0,
+    'TFK_number' : 750,
+    'initiator_number' : 62,
+     },
+     
+     {
+    'monomer_number' : 15000,
+    'crosslinker_number' : 3750,
+    'TTC_number' : 0,
+    'TFK_number' : 1250,
+    'initiator_number' : 62,
+    'r1' : 1,
+    'r2' : 1
+     },
+     
+    
+    {
+    'monomer_number' : 15000,
+    'crosslinker_number' : 5000,
+    'TTC_number' : 250,
+    'TFK_number' : 0,
+    'initiator_number' : 62,
+     },
+                  
+     {
+    'monomer_number' : 15000,
+    'crosslinker_number' : 5000,
+    'TTC_number' : 750,
+    'TFK_number' : 0,
+    'initiator_number' : 62,
+     },
+     
+     {
+    'monomer_number' : 15000,
+    'crosslinker_number' : 5000,
+    'TTC_number' : 1250,
+    'TFK_number' : 0,
     'initiator_number' : 62,
     'r1' : 1,
     'r2' : 1
      },
     
-    {
-    'monomer_number' : 1500,
-    'crosslinker_number' : 375,
-    'TTC_number' : 0,
-    'TFK_number' : 125,
-    'initiator_number' : 62,
-    'r1' : 1,
-    'r2' : 1
-     },
+    
+
+#     {
+#     'monomer_number' : 1500,
+#     'crosslinker_number' : 500,
+#     'TTC_number' : 0,
+#     'TFK_number' : 0,
+#     'initiator_number' : 6,
+#      },
+      
+#      {
+#     'monomer_number' : 1500,
+#     'crosslinker_number' : 475,
+#     'TTC_number' : 0,
+#     'TFK_number' : 25,
+#     'initiator_number' : 6,
+#      },
+                  
+#      {
+#     'monomer_number' : 1500,
+#     'crosslinker_number' : 425,
+#     'TTC_number' : 0,
+#     'TFK_number' : 75,
+#     'initiator_number' : 6,
+#      },
+                               
+#      {
+#     'monomer_number' : 1500,
+#     'crosslinker_number' : 375,
+#     'TTC_number' : 0,
+#     'TFK_number' : 125,
+#     'initiator_number' : 6,
+#      },
+    
+      
+#      {
+#     'monomer_number' : 1500,
+#     'crosslinker_number' : 500,
+#     'TTC_number' : 25,
+#     'TFK_number' : 0,
+#     'initiator_number' : 6,
+#      },
+                  
+#      {
+#     'monomer_number' : 1500,
+#     'crosslinker_number' : 500,
+#     'TTC_number' : 75,
+#     'TFK_number' : 0,
+#     'initiator_number' : 6,
+#      },
+                               
+#      {
+#     'monomer_number' : 1500,
+#     'crosslinker_number' : 500,
+#     'TTC_number' : 125,
+#     'TFK_number' : 0,
+#     'initiator_number' : 6,
+#      }
+
 
      ]
     
@@ -132,12 +240,15 @@ def batch_init(exprmnt, current_dict):
 
 class chain:
     
-    def __init__ (self, head, tail, head_type = None):
+    def __init__ (self, head, tail, head_type = None, elastic = True):
         self.kind = 'chain' #both 'type' and 'id' are reserved...
         self.component = []
+        self.neighbors = []
         self.head = head
         self.tail = tail
         self.head_type = head_type
+        self.head_length = 0
+        self.elastic = elastic
             
     def activate(self):
         self.tail = 'radical'
@@ -171,6 +282,7 @@ class crosslinker:
         self.monomer1 = monomer1
         self.monomer2 = monomer2
         self.elastic = elastic
+        self.length = crosslinker_length
         
 class monomer:
     def __init__(self, kind, free = True, parent = None, chain = None): #"type" can be used to denote different kinds of polymerizable groups, e.g. acrylate/styrene copolymerization
@@ -389,6 +501,10 @@ def MW_Calculator():
      #   print (chain.tail)
         if len(chain.component) == 0:
             continue
+            
+        dict_write(chain_length, {len(chain.component) : 1})
+        chain_length_list.append(len(chain.component))
+        
         w += len(chain.component)
         wsq += len(chain.component)*len(chain.component)
         n += 1
@@ -397,7 +513,7 @@ def MW_Calculator():
     mn = w/n
     print (mw, mn, mw/mn)
     print(len(chain_list))
-    return mw/mn
+    return mn, mw/mn
 
 def dict_write(master_dict, new_dict):
     for key in new_dict:
@@ -406,73 +522,248 @@ def dict_write(master_dict, new_dict):
         else:
             master_dict[key] = new_dict[key]
             
+            
+def chain_neighbors(chain_list):
+    
+    for chain0 in chain_list:
+        loops = 0
+        neighbors_list = []
+        if chain0.head_type == 'monomer' and chain0.head.free == False:
+            neighbors_list.append(chain0.head.chain)
+            if chain0.head.chain == chain0:
+                loops += 1
+        for monomer in chain0.component:
+            if type(monomer.parent) == chain:
+                neighbors_list.append(monomer.parent)
+            if type(monomer.parent) == crosslinker:
+                if monomer.parent.monomer1.free == True or monomer.parent.monomer2.free == True:
+                    continue
+                else:
+                    if monomer.parent.monomer1.chain == monomer.parent.monomer2.chain and monomer.parent.monomer1.chain == chain0:
+                        loops += 1
+                    neighbors_list.append(monomer.parent.monomer2.chain)
+                    neighbors_list.append(monomer.parent.monomer1.chain)
+                        
+        if type(chain0.tail) == TFK or type(chain0.tail) == TTC:
+            if chain0.tail.chain1 == chain0:
+                neighbors_list.append(chain0.tail.chain2)
+            else:
+                neighbors_list.append(chain0.tail.chain1)
+        
+        neighbors_list = list(set(neighbors_list))
+        
+        if chain0 in neighbors_list:
+            neighbors_list.remove(chain0)
+            
+        #print(len(neighbors_list))
+        
+        chain0.neighbors = neighbors_list
+        #print("loops", loops)
+        
+    for chain0 in chain_list:
+        #print(len(chain0.neighbors))
+        if len(chain0.neighbors) < 2:
+            chain0.elastic =False
+    
+    removed_chains = 1
+    while removed_chains > 0:
+        removed_chains = 0
+        for chain0 in chain_list:
+            if chain0.elastic == False:
+                continue
+            
+            elastic_connections = len(chain0.neighbors)
+            
+            for neighbor in chain0.neighbors:
+                if neighbor.elastic == False:
+                    elastic_connections -= 1
+            if elastic_connections < 2:
+                chain0.elastic = False
+                removed_chains += 1
+        #print('removed_chains', removed_chains)
+
+def crosslinker_elastic(crosslinker_list, chain_list):
+    
+    elastic_crosslinkers = 0
+    
+    for crosslinker0 in crosslinker_list:
+        if crosslinker0.monomer1.free == True or crosslinker0.monomer2.free == True:
+            crosslinker0.elastic = False
+            continue
+        elif crosslinker0.monomer1.chain.elastic == False or crosslinker0.monomer2.chain.elastic == False:
+            crosslinker0.elastic = False
+    
+    for chain0 in chain_list:
+        primary_loops = 0
+        last_crosslink = chain0
+        
+        for monomer in chain0.component:
+            if monomer.parent == None:
+                continue
+            #print(last_crosslink)
+            #print(monomer.parent)
+            
+            if monomer.parent == last_crosslink:
+                primary_loops += 1
+                if type(monomer.parent) == crosslinker:
+                    monomer.parent.elastic = False
+            
+        #print(primary_loops)
+    
+    elastic_crosslinker_list = []
+    for crosslinker0 in crosslinker_list:
+        if crosslinker0.elastic != False:
+            elastic_crosslinkers+=1
+            elastic_crosslinker_list.append(crosslinker0)
+    
+    #return ({crosslinker_length:elastic_crosslinkers})
+    return(elastic_crosslinker_list)
+        
+
+def xlinker_at_terminal(elastic_crosslinker_list, chain_list): 
+    
+#considers the first and last crosslinking point on a chain which are not true crosslinking points as only two chains join
+    
+    chain_end_dict = {}
+    
+    for chain0 in chain_list:        
+        if chain0.head_type == 'monomer' and chain0.head.free == False:
+            chain0.head_length = TFK_arm_length
+        else:
+            #print('length',len(chain0.component))
+            buffer = 0
+            elastic_active = 0
+            for monomer in chain0.component:
+                
+                if elastic_active > 2: break
+                if elastic_active == 1:
+                    buffer += Repeat_unit_length
+                    
+                if hasattr(monomer.parent, 'elastic') and monomer.parent.elastic != False:
+                    elastic_active += 1
+                    if elastic_active == 2:
+                        chain_end_dict[monomer] = buffer
+                        break
+            
+        if type(chain0.tail) == TFK or type(chain0.tail) == TTC:
+            continue
+        if len(chain0.neighbors) <= 2:
+            continue
+        else:
+            buffer = 0
+            elastic_active = 0
+            for monomer in reversed(chain0.component):
+                
+                if elastic_active > 2: break
+                if elastic_active == 1:
+                    buffer += Repeat_unit_length
+                    
+                if hasattr(monomer.parent, 'elastic') and monomer.parent.elastic != False:
+                    elastic_active += 1
+                    if elastic_active == 2:
+                        chain_end_dict[monomer] = buffer
+                        break
+    
+    
+    
+    #print(len(chain_end_dict))
+    for chain0 in chain_list:
+         if chain0.head_type == 'monomer' and chain0.head.free == False:
+             
+             if chain0.head in chain_end_dict:
+                chain0.head_length += chain_end_dict[chain0.head]
+                
+    elastic_crosslinkers_dict = {}
+    
+    for crosslinker0 in elastic_crosslinker_list:
+        
+        if crosslinker0.monomer1 in chain_end_dict:
+            crosslinker0.length += chain_end_dict[crosslinker0.monomer1]
+        
+        if crosslinker0.monomer2 in chain_end_dict:
+            crosslinker0.length += chain_end_dict[crosslinker0.monomer2]
+            
+        dict_write(elastic_crosslinkers_dict, {crosslinker0.length :1})
+        
+    drop_list = list(chain_end_dict.values())
+    #print(drop_list)
+    
+    return elastic_crosslinkers_dict,  drop_list
+
+        
 
 def MW_btw_Xlink_Calc(crosslinker, TFK, TTC, monomer):
     MW_btw_Xlink = {}
      
-        
-    for crosslinker in crosslinker_list:
-        if crosslinker.monomer1.free == True or crosslinker.monomer2.free == True:
-            
-            crosslinker.elastic = False
+    chain_neighbors(chain_list)
+    elastic_crosslinker_list = crosslinker_elastic(crosslinker_list, chain_list)
+    elastic_crosslinkers_dict, drop_list = xlinker_at_terminal(elastic_crosslinker_list, chain_list)
+    
+    dict_write(MW_btw_Xlink, elastic_crosslinkers_dict)
+    
+    for chain0 in chain_list:
+
+        if chain0.elastic == False:
             continue
-        buffer = crosslinker_length
-        dict_write(MW_btw_Xlink,{buffer:1})
-        
-   
-    for chain in chain_list:
-        
+        #print('length', len(chain0.component), 'neighbors',len(chain0.neighbors))
         elastic_active = False
         buffer = 0
         
-        if chain.head_type == 'monomer' and chain.head.free == False:
+        if chain0.head_type == 'monomer' and chain0.head.free == False:
             elastic_active = True
-            buffer += TFK_arm_length
+            buffer += chain0.head_length
+        a=0
+        b=0
+        c=0
+        d=0
+
         
-        for monomer in chain.component:
-           
+        for monomer in chain0.component:
+        
             buffer += Repeat_unit_length
-            
+            #print(monomer.parent)
             if monomer.parent == None:
-                
+                a+=1
                 continue
             
-    
+            elif hasattr(monomer.parent, 'elastic') and monomer.parent.elastic == False:
+                
+                #print(monomer.parent)
+                #if monomer.parent.elastic == False:
+                b+=1
+                continue
             
             elif elastic_active == True:
-                
+                c+=1
                 dict_write(MW_btw_Xlink,{buffer:1})
                 buffer = 0
 
             else:
-                
+                d+=1
                 elastic_active = True
                 buffer = 0
         
-        if type(chain.tail) == TFK:
-            chain.tail.flank1 += buffer
-            if elastic_active == False:
-                chain.tail.elastic = -1
-                        
-        elif type(chain.tail) == TTC:
-            chain.tail.flank1 += buffer
-            if elastic_active == False:
-                chain.tail.elastic = -1
+        #print('a=',a,'b=',b,'c=',c,'d=',d, )
+        
+        if type(chain0.tail) == TFK or type(chain0.tail) == TTC:
+            chain0.tail.flank1 += buffer
+            if chain0.elastic == False:
+                chain0.tail.elastic = -1
+    
+    for value in drop_list:
+        dict_write(MW_btw_Xlink,{value: -1})
 
     for TFK in TFK_list:
         if TFK.elastic == -1 :
-            continue
-        
-        buffer = TFK_TTC_length + TFK.flank1 + TFK.flank2
-
+            continue   
+        buffer = TFK_TTC_length + TFK.flank1
         dict_write(MW_btw_Xlink,{buffer:1})   
         
     for TTC in TTC_list:
         if TTC.elastic == -1 :
-
             continue
-        
-        buffer = TTC_length + TTC.flank1 + TTC.flank2
+    
+        buffer = TTC_length + TTC.flank1
         dict_write(MW_btw_Xlink,{buffer:1})
             
     return MW_btw_Xlink
@@ -482,7 +773,7 @@ def Avg_XL_per_Chain():
     xlink_number_by_mw = 0
     chain_number = 0
     monomer_number = 0
-    print('chain number', len(chain_list))
+    #print('chain number', len(chain_list))
     for chain in chain_list:   
         
         chain_xlink_number = 0
@@ -526,11 +817,20 @@ def Avg_XL_per_Chain():
                 
     
     
+all_data = []
+all_chain_lengths = []
 
+all_chain_list = []
+all_mw_data = []
 
 for exprmnt in batch_exprmnts:
     pdi = 0
     data = {}
+    chain_length = {}
+    chain_length_list= []
+    chain_list_list = []
+    mw_data = []
+    
     Avg_XL = {"xlink_number_by_mw":0, "monomer_number":0,"chain_number":0}
     batch_init(exprmnt, current_parameters)
     for repeat in range(repeats):
@@ -538,12 +838,28 @@ for exprmnt in batch_exprmnts:
 
         simulation()
         
+        pdi = MW_Calculator()
         MW_btw_Xlink = MW_btw_Xlink_Calc(crosslinker, TFK, TTC, monomer)
         dict_write(data,MW_btw_Xlink)
         
+        total = 0
+        segments = 0
+        sqr = 0
+        for i in MW_btw_Xlink:
+            sqr += data[i] * i * i 
+            total += data[i] * i
+            segments += data[i]
+            
+        print('1Mn_btw_Xlinks = ' , total/segments)
+        print('1Mw_btw_Xlinks = ' , sqr/total)
+        print("1Total MW = ", total) 
+    
+        chain_list_list.append(chain_list.copy())
+        mw_data.append({'mn':total/segments, 'mw':sqr/total, 'total_mw':total})
+        
         dict_write(Avg_XL, Avg_XL_per_Chain())
         #print(Avg_XL, Avg_XL['xlink_number_by_mw']/Avg_XL['chain_number'])
-    
+
     total = 0
     segments = 0
     sqr = 0
@@ -556,12 +872,18 @@ for exprmnt in batch_exprmnts:
     print('Mw_btw_Xlinks = ' , sqr/total)
     print("Total Segments = ", segments) 
     print("Total MW = ", total) 
+    print('pdi=', pdi)
+    
+    all_chain_lengths.append(chain_length_list)
+    all_chain_list.append(chain_list_list)
+    all_mw_data.append(mw_data)
     
     import matplotlib.pylab as plt
     
     lists = sorted(data.items()) # sorted by key, return a list of tuples
     x, y = zip(*lists) # unpack a list of pairs into two tuples
     plt.figure(dpi=300)
+    all_data.append([x,y])
     plt.bar(x, y)
     plt.show()
     
@@ -575,4 +897,3 @@ for exprmnt in batch_exprmnts:
         write = csv.writer(file) 
         write.writerows([['monomer = ', monomer_number - 2 * (crosslinker_number + TFK_number)],['crosslinker = ', crosslinker_number],['TTC = ', TTC_number],['TFK = ', TFK_number],['initiator = ', initiator_number],['r1 = ', r1],['r2 = ', r2]])
         write.writerows(lists)
-    
